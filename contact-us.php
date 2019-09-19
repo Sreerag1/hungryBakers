667f3880092ba2aa501732e89139f5b49b234465
Sreerag1 <sreeragnair19.sn@gmail.com>
2019-09-19 22:50:57 +0530

    Added the contact us and about us pages

---
+++ contact-us.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/breads.css">
+        <link rel="stylesheet" type="text/css" href="css/about-us.css">
+    </head>
+    <body>
+        <div class="main-container">
+            <div class="page-container">
+                <div class="shop-name"><img src="images/HeaderHome.png"></div>
+                <hr>
+                <nav class="header-navigation">
+                    <div class="left-nav">
+                        <a href="home.html">HOME</a>
+                        <a href="">ABOUT US</a>
+                        <a href="">ORDER NOW</a>
+                        <a href="">CONTACT US</a>
+                    </div>
+                    <div class="right-nav">
+                        <a href="">SIGN UP</a>
+                        <a>|</a>
+                        <a href="">LOGIN</a>
+                        <a>|</a>
+                        <a href="">CART</a>
+                        <a href=""><img src="images/emptybasket.png"></a>
+                    </div>
+                </nav>
+
+                 <!-- Breads container -->
+        <div class="aboutus-container">
+                    <div class="GETIN">
+                        <p>GET IN TOUCH</p>
+                    </div>
+                    <div class="texts">
+                        <input type="text"  class="form-input" value="Your name" ><br>
+                        <br>
+                        <input type="text" class="form-input" value="Your email"><br>
+                        <br>
+                        <input type="text" class="form-input" value="Your message"><br>
+                        <br>
+                        <input type="submit" class="form-input" value="submit">
+                    </div>
+                    <div class="GETQ" style="">
+                        <p>GOT A QUESTION</p>
+                    </div>
+                    <div class="answer" >
+                        <p>We'd love to answer your call !</p>
+                    </div>
+                    <div class="answer">
+                        <p>Tel : 020-26662666</p>
+                    </div>
+                    <div class="address">
+                        <p>----------------------------------------------------------------------------------------------------<p>
+                    </div>
+                    <div class="address">
+                        <p>Address</p>
+                    </div>
+                    <div class="address">
+                        <p>84,M.G.Road</p>
+                    </div>
+                    <div class="address">
+                        <p>Pune 411014</p>
+                    </div>
+                </div>
+                <!-- end of aboutus container -->
+                <footer>
+                    <div class="footer-container">
+                        <img src="images/Sidfooter.png" width="100%" height="155" alt="footer" usemap="#socialMediaLinks">
+                        
+                    </div>
+                    <map name="socialMediaLinks">
+                    <area shape="rect" coords="822,72,864,118" alt="Sun" href="https://www.facebook.com" target="_blank">
+                        <area shape="rect" coords="888,72,923,118" alt="Mercury" href="https://www.instagram.com" target="_blank">
+                            <area shape="rect" coords="943,72,981,118" alt="Venus" href="https://www.snapchat.com" target="_blank">
+                                </map>
+                            </footer>
+                        </div>
+                        <!-- Page container -->
+                    </div>
+                    <!-- Vertical main column container -->
+                </body>
+            </html>
