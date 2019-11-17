4ce870e63849ce2b946aa56d6a49db4570b5c98b
Sreerag1 <sreeragnair19.sn@gmail.com>
2019-11-17 20:04:07 +0530

    Bug fix cart total and gst

--- cart.php
+++ cart.php
@@ -6,8 +6,8 @@ require "connection.php";
 // $sth = $conn->prepare("SELECT * FROM masteritemlist where category ='".$category."'");
 // var_dump($cart);
 $cart = unserialize($_SESSION["cart"]);
-echo "displaying cart";
-$cart->displayCart();
+// echo "displaying cart";
+// $cart->displayCart();
 $itemIds = array_keys($cart->items);
 if (empty($itemIds)) {
     echo "<div class='cart-empty'><h1>Your cart is empty!</h1></div>";
--- class-cart.php
+++ class-cart.php
@@ -25,7 +25,7 @@ class Cart
     public function addItem($itemId, $quantity = 1)
     {
         if (array_key_exists($itemId, $this->items)) {
-            $this->items[$itemId] =$this->items[$itemId] + $quantity;
+            $this->items[$itemId] = $this->items[$itemId] + $quantity;
         } else {
             $this->items[$itemId] =$quantity;
         }
@@ -33,13 +33,20 @@ class Cart
         require "connection.php";
         $sth = $conn->prepare("SELECT price FROM masteritemlist where id = $itemId");
         $sth->execute();
-        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        $itemInfo = $sth->fetch(PDO::FETCH_ASSOC);
         error_log("class");
-        error_log(print_r($row, 1));
-        $this->totalCost = $this->totalCost + $quantity*$row['price'];
-        $this->totalGst = $this->totalCost * $this->gstPercent/100;
-        $this->totalCost += $this->totalCost + $this->totalGst;
-        return $this->items;
+        error_log(print_r($itemInfo, 1));
+        //Calculate Item GST
+        $itemGst = $quantity*$itemInfo['price'] * $this->gstPercent/100;
+        error_log("itemGst");
+        error_log($itemGst);
+        $this->totalGst = $this->totalGst + $itemGst;
+        $this->totalCost = $this->totalCost + $quantity*$itemInfo['price'] +$itemGst;
+        error_log("itemGst");
+        error_log($itemGst);
+        
+        error_log(print_r($this, 1));
+        return $this->totalItems;
     }
     public function deleteItem($itemId = '')
     {
@@ -48,10 +55,12 @@ class Cart
         require "connection.php";
         $sth = $conn->prepare("SELECT price FROM masteritemlist where id = $itemId");
         $sth->execute();
-        $row = $sth->fetch(PDO::FETCH_ASSOC);
+        $itemInfo = $sth->fetch(PDO::FETCH_ASSOC);
         error_log("class");
-        error_log(print_r($row, 1));
-        $this->totalCost = $this->totalCost - $this->items[$itemId]*$row['price'];
+        error_log(print_r($itemInfo, 1));
+        $itemGst= $itemInfo['price'] *  $this->gstPercent/100;
+        $this->totalGst = $this->totalGst - $itemGst;
+        $this->totalCost = $this->totalCost - $this->items[$itemId]*$itemInfo['price'] - $itemGst;
         unset($this->items[$itemId]);
         error_log(print_r($this->items, 1));
         return true;
@@ -67,7 +76,7 @@ class Cart
         echo "All items:";
         print_r($this->items);
         echo "Item Total:".$this->totalItems."<br>";
-        echo "Item Cost:".$this->totalCost."<br>";
+        echo "Cart total Cost:".$this->totalCost."<br>";
         echo "Total Gst Percent:".$this->gstPercent."<br>";
         echo "Total Gst:".$this->totalGst."<br>";
         echo "</pre>";
